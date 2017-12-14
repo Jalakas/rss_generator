8506d521db25b00235a43f6766cfe4f1886e6507
Jalakas <anari.jalakas@gmail.com>
2017-12-14 06:24:31 +0200

    autopep8, flake8, rename parser.py to te_parser.py

--- rssmaker.py
+++ rssmaker.py
@@ -7,12 +7,13 @@
 
 from lxml import etree
 
+
 def rssmaker(dataset):
     version = "2.0"
-    root = etree.Element(   
+    root = etree.Element(
         "rss",
-        version = version)
-    
+        version=version)
+
     channel = etree.SubElement(root, "channel")
     title = etree.SubElement(channel, "title")
     title.text = 'Tartu Ekspress'
@@ -20,19 +21,18 @@ def rssmaker(dataset):
     link.text = 'http://tartuekspress.ee/?page=20&type=3'
     description = etree.SubElement(channel, "description")
     description.text = 'Tartu Ekspress - Kõik uudised'
-    
+
     for i in enumerate(dataset['articleIds']):
         item = etree.SubElement(channel, "item")
         item_title = etree.SubElement(item, "title")
-        item_title.text = list(dataset['articleTitles'])[i[0]].encode('ascii', 'xmlcharrefreplace')
-        
+        item_title.text = list(
+            dataset['articleTitles'])[i[0]].encode('ascii', 'xmlcharrefreplace')
+
         item_link = etree.SubElement(item, "link")
-        item_link.text = list(dataset['articleUrls'])[i[0]].encode('ascii', 'xmlcharrefreplace')
-        
+        item_link.text = list(
+            dataset['articleUrls'])[i[0]].encode('ascii', 'xmlcharrefreplace')
+
         item_description = etree.SubElement(item, "description")
         item_description.text = list(dataset['articleHeaders'])[i[0]].encode('ascii', 'xmlcharrefreplace')
 
     return(etree.ElementTree(root))
-
-
-    
--- te_parser.py
+++ te_parser.py
@@ -5,30 +5,44 @@
     Tartu Ekspressi RSS-voo loomiseks
 """
 
-from lxml import etree
+# from lxml import etree
 from lxml import html
 import makereq
 
 
 def getArticleData(articleURL):
-    """ Artikli lehe pärimine """
+    """
+    Artikli lehe pärimine
+    """
     aricleDataHtml = makereq.makeReq(articleURL)
     treeArt = html.fromstring(aricleDataHtml)
     return treeArt
 
 
 def extractArticleHeader(tree):
-    """ Artikli päise saamiseks """
-    return next(iter(tree.xpath('//div[@class="full_width"]/p[1]/strong/text()') or []), None)
+    """
+    Artikli päise saamiseks
+    """
+    return next(
+        iter(
+            tree.xpath('//div[@class="full_width"]/p[1]/strong/text()') or []),
+        None)
 
 
 def extractArticleImage(tree):
-    """ Artikli pildi aadressi saamiseks """
-    return next(iter(tree.xpath('//div[@class="full_width"]/a/img[@class="thumb"]/@src') or []), None)
-    
-    
+    """
+    Artikli pildi aadressi saamiseks
+    """
+    return next(
+        iter(
+            tree.xpath('//div[@class="full_width"]/a/img[@class="thumb"]/@src') or []),
+        None)
+
+
 def extractArticleBody(tree):
-    """ Artikli tervikteksti saamiseks """
+    """
+    Artikli tervikteksti saamiseks
+    """
     body = tree.xpath('//div[@class="full_width"]/p')
     fulltext = []
     for elem in body:
@@ -39,10 +53,12 @@ def extractArticleBody(tree):
             None
         fulltext.append(rawtext)
     return ''.join(fulltext)
-    
 
-def getNewsList(newshtml,domain):
-    """ Peameetod kõigi uudiste nimekirja loomiseks """
+
+def getNewsList(newshtml, domain):
+    """
+    Peameetod kõigi uudiste nimekirja loomiseks
+    """
     tree = html.fromstring(newshtml)
     headLines = tree.xpath('//div[@class="forum"][2]/ul/li/a/text()')
     newsUrls = tree.xpath('//div[@class="forum"][2]/ul/li/a/@href')
@@ -50,21 +66,22 @@ def getNewsList(newshtml,domain):
     articleHeaders = []
     articleImages = []
     articleBodys = []
-    newsUrls = [domain+elem for elem in newsUrls]
-    #newsUrls = list(newsUrls[:1])
-    #headLines = list(headLines[:1])
+    newsUrls = [domain + elem for elem in newsUrls]
+    # newsUrls = list(newsUrls[:1])
+    # headLines = list(headLines[:1])
 
     for elem in newsUrls:
-        articleIds.append(elem[elem.index('&id=')+4:elem.index('&',elem.index('&id=')+4)])
+        articleIds.append(
+            elem[elem.index('&id=') + 4:elem.index('&', elem.index('&id=') + 4)])
         articleTree = getArticleData(elem)
-        
+
         articleHeaders.append(extractArticleHeader(articleTree))
         articleImages.append(extractArticleImage(articleTree))
         articleBodys.append(extractArticleBody(articleTree))
 
-    return {"articleIds" : articleIds,
-            "articleUrls" : newsUrls,
-            "articleTitles" : headLines,
-            "articleHeaders" : articleHeaders,
-            "articleImages" : articleImages,
-            "articleBodys" : articleBodys}
+    return {"articleIds": articleIds,
+            "articleUrls": newsUrls,
+            "articleTitles": headLines,
+            "articleHeaders": articleHeaders,
+            "articleImages": articleImages,
+            "articleBodys": articleBodys}
